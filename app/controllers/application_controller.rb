502949e592b7e2411ccad868c708fc9d93a5f0dc
charleslapeyre <lapeyrecc@gmail.com>
2017-03-07 11:06:55 -0600

    Setup facebook sign-in and sign-out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  # Declare a helper method and make it available to controllers and views
+  helper_method :current_user
+
+  private
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
 end
