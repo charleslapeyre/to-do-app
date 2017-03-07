d11a5f8c9a102bbef0bdb961cc132f151b6e7678
charleslapeyre <lapeyrecc@gmail.com>
2017-03-07 09:42:21 -0600

    setup omniauth table and model *setup up parsing of Facebook API hash *create method for getting user avatar

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170306144934) do
+ActiveRecord::Schema.define(version: 20170307152531) do
 
   create_table "user_tasks", force: :cascade do |t|
     t.string   "description"
     t.date     "due"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
